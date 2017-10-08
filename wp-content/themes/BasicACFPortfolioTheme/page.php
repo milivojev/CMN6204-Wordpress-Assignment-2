d8d980ec104092d7622a2e44b39f3bc59f86c1c5
milivojev <vas.milivoj@gmail.com>
2017-10-09 00:33:28 +0200

    page.php created

---
+++ wp-content/themes/BasicACFPortfolioTheme/page.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+<?php the_post(); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading/Breadcrumbs -->
+      <h1 class="mt-4 mb-3">Full Width
+        <small>Subheading</small>
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.html">Home</a>
+        </li>
+        <li class="breadcrumb-item active">Full Width</li>
+      </ol>
+
+      <div class="content">
+       <?php the_content(); ?>
+      </div>
+      
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  </body>
+
+</html>
