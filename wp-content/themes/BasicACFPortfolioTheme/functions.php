fb21464b45aabae653935b0f581bb7af05cc1c93
milivojev <vas.milivoj@gmail.com>
2017-10-08 22:39:44 +0200

    Site title dinamicly set

--- wp-content/themes/BasicACFPortfolioTheme/functions.php
+++ wp-content/themes/BasicACFPortfolioTheme/functions.php
@@ -3,3 +3,11 @@ function custom_title(){
 	return wp_title(). ' | '. get_bloginfo('name').' ~ '.get_bloginfo('description');
 }
 
+add_action('init','register_menus');
+
+function register_menus(){
+	register_nav_menus([
+		'Header Menu'=>'main menu for the top of the page'
+	]);
+}
+
--- wp-content/themes/BasicACFPortfolioTheme/header.php
+++ wp-content/themes/BasicACFPortfolioTheme/header.php
@@ -23,9 +23,9 @@
     <!-- Navigation -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <a class="navbar-brand" href="index.html"><?= get_bloginfo('name'); ?></a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon">miki</span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
